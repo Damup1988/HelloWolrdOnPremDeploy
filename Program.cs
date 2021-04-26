c0aeecc3e41983c38537415cf9b1aa243950a2fd
Damup1988 <damup1988@gmail.com>
2021-04-27 00:34:32 +0300

    the message has been changed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace HelloWorldOnPremDeploy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wolrd!");
+            Console.WriteLine("Hello Damir!");
+            Console.ReadLine();
         }
     }
 }
