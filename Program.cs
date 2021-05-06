f64ef5869940936f950cc029b610554d7f64df75
Damir Safarov <damup1988@gmail.com>
2021-05-07 02:05:53 +0300

    one func has been added

--- Program.cs
+++ Program.cs
@@ -4,9 +4,17 @@ namespace HelloWorldOnPremDeploy
 {
     class Program
     {
+        static void some_func()
+        {
+            Console.WriteLine("Some func has been called...");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Damir and World!");
+
+            some_func();
+
             Console.ReadLine();
         }
     }
